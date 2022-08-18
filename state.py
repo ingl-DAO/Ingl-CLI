5ad2cfd925237c0d26655a50cbac7d063b2c3da3
kamda-cyrial <42605286+kamda-cyrial@users.noreply.github.com>
2022-08-18 16:53:30 -0400

    state.py dupkey update and program_id update

--- state.py
+++ state.py
@@ -8,7 +8,7 @@ class Constants:
     INGL_MINT_AUTHORITY_KEY = "mint_authority"
     INGL_MINTING_POOL_KEY = "minting_pool"
     COLLECTION_HOLDER_KEY = "collection_holder"
-    INGL_PROGRAM_ID = PublicKey("41z2kpMac1RpH5XnBoKnY6vjmJwdbwc1aHRQszCgbyDv")
+    INGL_PROGRAM_ID = PublicKey("9dSZN479QxPdogZTwjaBRiTfFAvhq3kNF1GEwUWW7es6")
     STAKE_PROGRAM_ID = PublicKey("Stake11111111111111111111111111111111111111")
     GLOBAL_GEM_KEY = "global_gem_account"
     GEM_ACCOUNT_CONST = "gem_account"
@@ -25,6 +25,7 @@ class Constants:
     VOTE_PROGRAM_ID = PublicKey("Vote111111111111111111111111111111111111111")
     T_STAKE_ACCOUNT_KEY = "Temporary_stake_account_key"
     T_WITHDRAW_KEY = "Temporary_withdraw"
+    DUPKEYBYTES = b"dupkey"
 
     VALIDATOR_ID_SHARE = 15
     TREASURY_SHARE = 13
@@ -77,6 +78,16 @@ InglVoteAccountData = CStruct(
     "is_t_stake_initialized" / Bool,
     "pending_delegation_total" / U64,
     "vote_rewards" / Vec(VoteRewards),
+)
+
+
+ValidatorProposal = CStruct(
+    "validation_phrase" / U32,
+    "validator_ids" / Vec(U8[32]),
+    "date_created" / U32,
+    "date_finalized" / Option(U32),
+    "votes" / Vec(U32),
+    "winner" / Option(U8[32]),
 )
 
 def private_key_from_json(filepath):
