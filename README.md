36a65d6fcbfbb68e7fb8989bfb53584b1b3b1419
kamda-cyrial <42605286+kamda-cyrial@users.noreply.github.com>
2023-01-20 07:02:40 -0500

    fixing the pubkey method display during program_id set and updating the readme

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Fractionalizing Validator Creation and Ownership</h3>
 
 ## 
 
-# CLI for interacting with Ingl Programs (Linux only Recommended)
+# CLI for interacting with Ingl Programs (Ubuntu 20.04 only is Recommended)
 ## Installation
 ### Isolating an environment to prevent current or future environmental conflicts:
 ```
--- src/state.py
+++ src/state.py
@@ -170,17 +170,13 @@ def parse_pubkey_input(str_input: str) -> PubkeyInput:
             pubkey = PubkeyInput(pubkey=Pubkey.from_string(str_input))
             return pubkey
         except Exception as e:
-            if 'invalid public key input:' in str(e):
-                try:
-                    t_keypair = keypair_from_json(str_input)
-                    return PubkeyInput(t_keypair=t_keypair)
-                except Exception as new_e:
-                    print("invalid public key input")
-                    raise new_e
-            else:
+            try:
+                t_keypair = keypair_from_json(str_input)
+                return PubkeyInput(t_keypair=t_keypair)
+            except Exception as new_e:
                 print("invalid public key input")
-                raise e
-
+                raise new_e
+            
 async def sign_and_send_tx(tx: Transaction, client: AsyncClient, *args) -> SendTransactionResp:
     last_valid_block_height = None
     if client.blockhash_cache:
@@ -288,5 +284,5 @@ def set_keypair_path(keypair_path: str) -> bool:
         return False
     set_config('keypair_path', keypair_path)
     print("Keypair path set to: ", path)
-    print("Keypair Public Key: ", keypair.pubkey)
+    print("Keypair Public Key: ", keypair.pubkey())
     return True
