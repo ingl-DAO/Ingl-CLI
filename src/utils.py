7281e05ca67ad447fffadc26f21cf8acbb1e8b9d
artemesian <angelodiepe10@yahoo.com>
2023-02-28 13:27:32 +0100

    added NFTData struct and updated undelegated to add interested proposals account metas

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from .state import *
 
+
 def parse_vote(vote: str) -> bool:
     vote = vote.lower()
     if vote == "approve" or vote == "a" or vote == "yes" or vote == "y":
@@ -7,7 +8,9 @@ def parse_vote(vote: str) -> bool:
     elif vote == "dissaprove" or vote == 'd' or vote == "no" or vote == "n":
         return False
     else:
-        raise ValueError("Vote can only be [a]pprove, [d]issaprove, [y]es or [n]o")
+        raise ValueError(
+            "Vote can only be [a]pprove, [d]issaprove, [y]es or [n]o")
+
 
 def parse_proposal(proposal: str) -> Tuple[Optional[PubkeyInput], Optional[int]]:
     try:
